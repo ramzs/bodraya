5c3642e8f573ba8c506d368b7986c4aa21adae89
Hiiragi0831 <glazunovden@yandex.ru>
2023-01-31 23:15:28 +0300

    проба пера анимации

---
+++ source/js/modules/paralax.js
@@ -0,0 +1,14 @@
+// import simpleParallax from 'simple-parallax-js';
+
+export const paralax = () => {
+
+
+  // new simpleParallax(document.querySelector('.history__decor--1'), {
+  //   orientation: "up",
+  //   // scale: 1,
+  //   delay: .6,
+  //   overflow: true,
+  //   transition: 'cubic-bezier(0,0,0,1)'
+  // });
+
+}
