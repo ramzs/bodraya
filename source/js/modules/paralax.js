00c645b64c8d162f9b7208e2a777e8b964280bd2
Hiiragi0831 <glazunovden@yandex.ru>
2023-02-01 21:24:17 +0300

    observer

--- source/js/modules/paralax.js
+++ source/js/modules/paralax.js
@@ -1,4 +1,8 @@
 import Rellax from 'rellax';
+import { gsap } from "gsap";
+import { Observer } from "gsap/Observer";
+
+gsap.registerPlugin(Observer);
 
 export const paralax = () => {
   const rellax = new Rellax('.parallax', {
@@ -9,4 +13,40 @@ export const paralax = () => {
     // vertical: true,
     // horizontal: false
   });
+
+  const onVisible = (element) => {
+    gsap.to(element, {
+      opacity: 1,
+      duration: 1.5,
+      ease: "power3.out"
+    });
+  }
+
+  const observerElements = document.querySelectorAll('.parallax');
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px 0px -100px 0px',
+    threshold: 0
+  };
+  const Observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach(entry => {
+        // animation targets
+        if (entry.intersectionRatio > 0) {
+          // Run GSAP animation tween.
+          onVisible(entry.target);
+          // remove the observer after it's triggered
+          Observer.unobserve(entry.target);
+        }
+      });
+    },
+    observerOptions
+  );
+
+  observerElements.forEach(el => {
+    Observer.observe(el);
+  });
 }
+
+
+
